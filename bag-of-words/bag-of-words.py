26c006deaa8ad3ff4e7072fac592c23f9379613d
Anand Raj <211809969+anan5093@users.noreply.github.com>
2026-03-19 01:53:56 +0530

    Add solution for bag-of-words

---
+++ bag-of-words/bag-of-words.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def bag_of_words_vector(tokens, vocab):
+    vocab_index = {word: i for i, word in enumerate(vocab)}
+
+    counts = np.zeros(len(vocab), dtype = int)
+
+    for token in tokens:
+        if token in vocab_index:
+            counts[vocab_index[token]] +=1
+    return counts       
+    
+   # pass
